cc1b7799e4d9ba7c5f72936b43ab7022e283a645
Shayane Alcantara <shayaneliebe@gmail.com>
2019-11-01 12:35:50 -0300

    Add pattern in 2 cases on create_adds

--- order_service/tests.py
+++ order_service/tests.py
@@ -1,17 +1,18 @@
 from django.test import TestCase
-from .models import Order
+from .models import Order, Adds
 from rest_framework.test import APITestCase
 import json
 from django.core import serializers
 from .serializers import OrderSerializer
-from .order_test_helper import create_order
+from .order_test_helper import create_order, create_adds
 from .order_test_helper import (default_description, default_name, 
-                                default_number_identification, default_pk, default_quantity)
+                                default_number_identification, default_pk, 
+                                default_quantity, adds_name, adds_number_identification, 
+                                adds_quantity, adds_description, adds_pk)
 
 class CheckOrderAPITest(APITestCase):
 
     def test_create_order_with_correct_params(self):
-        # If the order was successfully created
         data = create_order(default_name, default_number_identification, default_description, default_quantity)
         response = self.client.post('/create_order/', data=data)
         self.assertEqual(response.status_code, 200)
@@ -41,10 +42,12 @@ class CheckOrderAPITest(APITestCase):
         response3 = self.client.post('/delete_order/', data=data3, format='json')
         self.assertEqual(response3.status_code, 404)
 
+
     def test_delete_order_with_wrong_id(self):
         data = create_order(default_name, default_number_identification, default_pk, default_quantity)
         response = self.client.post('/delete_order/', data=data)
         self.assertEqual(response.status_code, 404)
+
 
     def test_list_orders(self):
         request_1 = {}
@@ -60,19 +63,23 @@ class CheckOrderAPITest(APITestCase):
 
 class CheckOrderAdds(APITestCase):
 
-    def test_create_order_with_correct_adds(self):
-        request_1 = {'adds_name':'brocolis', 'adds_number_identification':'1', 'adds_description':'Descrição topzera', 'adds_quantity':'2'}
-        response_1 = self.client.post('/create_adds/', request_1)
-        self.assertEqual(response_1.status_code, 200)
+    def test_create_adds_with_correct_adds(self):
+        data_adds = create_adds(adds_name, adds_number_identification, adds_description, adds_quantity)
+        response = self.client.post('/create_adds/', data_adds=data_adds)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_create_adds_with_status_0(number_identification, status=0):
+        Adds.objects.create(
+            adds_name = adds_name,
+            adds_description = adds_description,
+            adds_quantity = adds_quantity,
+            adds_number_identification = adds_number_identification)
 
     def test_delete_adds_with_wrong_id(self):
-        request_1 = {'adds_number_identification': '1', 'adds_name': 'Bad name', 'adds_description': 'Bad description', 'adds_quantity': '2'}
-        self.client.post('/delete_adds/', request_1)
-
-        # INTERNAL REQUEST ERROR if order does not exist
-        request_2 = {'number_identification': 'p'}
-        response_2 = self.client.post('/delete_adds/', request_2)
-        self.assertEqual(response_2.status_code, 404)
+        data = create_order(adds_name, adds_number_identification, adds_pk, adds_quantity)
+        response = self.client.post('/delete_adds/', data=data)
+        self.assertEqual(response.status_code, 404)
 
     def test_list_adds(self):
         request_1 = {}
